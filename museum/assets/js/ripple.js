65210e73f35b443fae91194def29b8eec1fe79a4
Vladimir <kusakinvova@gmail.com>
2021-09-29 18:46:55 +0400

    feat: form popup

---
+++ museum/assets/js/ripple.js
@@ -0,0 +1,26 @@
+export function ripple(className) {
+
+  const buttons = document.querySelectorAll(className);
+
+  buttons.forEach(button => {
+      button.addEventListener('click', function (e) {
+          const x = e.clientX;
+          const y = e.clientY;
+
+          const buttonTop = e.target.offsetTop;
+          const buttonLeft = e.target.offsetLeft;
+
+          const xInside = x - buttonLeft;
+          const yInside = y - buttonTop;
+
+          const circle = document.createElement('span');
+          circle.classList.add('circle');
+          circle.style.top = yInside + 'px';
+          circle.style.left = xInside + 'px';
+
+          this.appendChild(circle);
+
+          setTimeout(() => circle.remove(), 500);
+      })
+  });
+}
--- museum/assets/js/script.js
+++ museum/assets/js/script.js
@@ -1,9 +1,11 @@
 import { gallery } from './gallery.js';
 import { popup } from './popup.js';
 import { player } from './player.js';
+import { ripple } from './ripple.js';
 
 gallery();
 popup('.popup');
 player();
+ripple('.ripple');
 
 console.log("Hello World!");
