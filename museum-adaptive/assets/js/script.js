455b7d2b87c0fba126f7fd6a90280189a0b4cdcb
Vladimir <kusakinvova@gmail.com>
2021-10-05 01:24:44 +0400

    feat: mes

--- museum-adaptive/assets/js/script.js
+++ museum-adaptive/assets/js/script.js
@@ -10,4 +10,8 @@ player();
 ripple('.ripple');
 select('.buytform__selectstyle');
 
-console.log("Hello World!");
+let console_style = 'color: #ecb830; font-size: 14px';
+console.log('%c'+ 'Привет!', 'color: #008000; font-size: 24px');
+console.log('%c'+ 'Я не успел сделать работу к Дедлайну.', console_style);
+console.log('%c'+ 'Пожалуйста, если не сложно, проверь мою работу в последний день Кросчека.', console_style);
+console.log('%c'+ 'Мой телеграмм для связи https://t.me/kusakinvladimir ', console_style);
