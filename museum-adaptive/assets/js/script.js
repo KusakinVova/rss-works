97ed6a9adbc38d6ea514a5fc3076c0bfc6f21bc9
Vladimir <kusakinvova@gmail.com>
2021-10-07 19:24:26 +0400

    feat: apdate mes in console

--- museum-adaptive/assets/js/script.js
+++ museum-adaptive/assets/js/script.js
@@ -13,5 +13,6 @@ select('.buytform__selectstyle');
 let console_style = 'color: #ecb830; font-size: 14px';
 console.log('%c'+ 'Привет!', 'color: #008000; font-size: 24px');
 console.log('%c'+ 'Ты уже видел это сообщение и сегодня 07.10, дедлайн кросчека.', 'color: #008000; font-size: 24px');
-console.log('%c'+ 'Пожалуйста, если не сложно, проверь мою работу после 15 часов в последний день Кросчека.', console_style);
+console.log('%c'+ 'Сейчас делаю меню.', 'color: #008000; font-size: 24px');
+console.log('%c'+ 'Пожалуйста, если не сложно, проверь мою работу после 9 вечера в последний день Кросчека.', console_style);
 console.log('%c'+ 'Мой телеграмм для связи https://t.me/kusakinvladimir ', console_style);
