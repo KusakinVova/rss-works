ff6b707cceb0811183721c4940fe8b633e893ea7
Vladimir <kusakinvova@gmail.com>
2021-10-07 09:35:49 +0400

    fix: adaptive

--- museum-adaptive/assets/js/script.js
+++ museum-adaptive/assets/js/script.js
@@ -12,6 +12,6 @@ select('.buytform__selectstyle');
 
 let console_style = 'color: #ecb830; font-size: 14px';
 console.log('%c'+ 'Привет!', 'color: #008000; font-size: 24px');
-console.log('%c'+ 'Я не успел сделать работу к Дедлайну.', console_style);
-console.log('%c'+ 'Пожалуйста, если не сложно, проверь мою работу в последний день Кросчека.', console_style);
+console.log('%c'+ 'Ты уже видел это сообщение и сегодня 07.10, дедлайн кросчека.', 'color: #008000; font-size: 24px');
+console.log('%c'+ 'Пожалуйста, если не сложно, проверь мою работу после 15 часов в последний день Кросчека.', console_style);
 console.log('%c'+ 'Мой телеграмм для связи https://t.me/kusakinvladimir ', console_style);
