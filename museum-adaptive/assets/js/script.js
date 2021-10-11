ef4eb2113ced07cd0b5023b8fe9689bef04538cc
Vladimir <kusakinvova@gmail.com>
2021-10-11 18:56:10 +0400

    feat: adaptive-menu (2)

--- museum-adaptive/assets/js/script.js
+++ museum-adaptive/assets/js/script.js
@@ -12,9 +12,9 @@ ripple('.ripple');
 menu('.headernav', '.headernav__burger', '.welcome__info');
 select('.buytform__selectstyle');
 
-let console_style = 'color: #ecb830; font-size: 14px';
-console.log('%c'+ 'Привет!', 'color: #008000; font-size: 24px');
-console.log('%c'+ 'Ты уже видел это сообщение и сегодня 07.10, дедлайн кросчека.', 'color: #008000; font-size: 24px');
-console.log('%c'+ 'Сейчас делаю меню.', 'color: #008000; font-size: 24px');
-console.log('%c'+ 'Пожалуйста, если не сложно, проверь мою работу после 9 вечера в последний день Кросчека.', console_style);
-console.log('%c'+ 'Мой телеграмм для связи https://t.me/kusakinvladimir ', console_style);
+// let console_style = 'color: #ecb830; font-size: 14px';
+// console.log('%c'+ 'Привет!', 'color: #008000; font-size: 24px');
+// console.log('%c'+ 'Ты уже видел это сообщение и сегодня 07.10, дедлайн кросчека.', 'color: #008000; font-size: 24px');
+// console.log('%c'+ 'Сейчас делаю меню.', 'color: #008000; font-size: 24px');
+// console.log('%c'+ 'Пожалуйста, если не сложно, проверь мою работу после 9 вечера в последний день Кросчека.', console_style);
+// console.log('%c'+ 'Мой телеграмм для связи https://t.me/kusakinvladimir ', console_style);
