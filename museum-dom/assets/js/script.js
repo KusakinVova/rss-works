03e8bcf38d37d06f57587bbc4d25d33acddb2f8b
Vladimir <kusakinvova@gmail.com>
2021-10-12 03:18:37 +0400

    feat: console message

--- museum-dom/assets/js/script.js
+++ museum-dom/assets/js/script.js
@@ -90,12 +90,13 @@ function createMap(){
 }
 createMap();
 // --------------------------------------------
+// explorer line
 
 
 
-// let console_style = 'color: #ecb830; font-size: 14px';
-// console.log('%c'+ 'Привет!', 'color: #008000; font-size: 24px');
-// console.log('%c'+ 'Ты уже видел это сообщение и сегодня 07.10, дедлайн кросчека.', 'color: #008000; font-size: 24px');
-// console.log('%c'+ 'Сейчас делаю меню.', 'color: #008000; font-size: 24px');
-// console.log('%c'+ 'Пожалуйста, если не сложно, проверь мою работу после 9 вечера в последний день Кросчека.', console_style);
-// console.log('%c'+ 'Мой телеграмм для связи https://t.me/kusakinvladimir ', console_style);
+
+console.log('%c'+ 'Привет!', 'color: #008000; font-size: 36px');
+console.log('%c'+ 'По дружески прошу проверить мою работу в последний день кросчека.', 'color: #008000; font-size: 32px');
+console.log('%c'+ 'Просто я из-за работы не успеваю сделать до дедлайна.', 'color: #008000; font-size: 28px');
+console.log('%c'+ 'Благодарю за понимание!', 'color: #008000; font-size: 36px');
+console.log('%c'+ 'Мой телеграмм для связи https://t.me/kusakinvladimir ', 'color: #008000; font-size: 24px');
