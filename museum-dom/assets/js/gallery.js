edf0b7650c6ecafa22d40e3df4e57e505fb3c13b
Vladimir <kusakinvova@gmail.com>
2021-10-12 21:04:12 +0400

    feat: add gallery-animation

--- museum-dom/assets/js/gallery.js
+++ museum-dom/assets/js/gallery.js
@@ -28,7 +28,46 @@ export function gallery() {
       }
     })
   }
-
+  getGalleryCoordinats();
   setTimeout(getGalleryCoordinats, 3000);
+
+  //---------------------------------
+  const animItems = document.querySelectorAll(".gallery__item");
+
+  if(animItems.length > 0 ){
+    window.addEventListener('scroll', animScroll);
+    function animScroll(argument) {
+      for (let index = 0; index < animItems.length; index++) {
+        const animItem = animItems[index];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        const animStart = 10;
+
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+        if( animItemHeight > window.innerHeight){
+          animItemPoint = window.innerHeight - window.innerHeight / animStart
+        }
+
+        if( (scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight) ){
+          animItem.classList.add('show');
+        }
+        else{
+          animItem.classList.remove('show');
+        }
+
+      }
+    }
+
+    function offset(el){
+      const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return {top: rect.top + scrollTop, left: rect.left + scrollLeft};
+    }
+    animScroll();
+  }
+
+  //---------------------------------
 
 }
