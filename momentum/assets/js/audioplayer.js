13aa77bad89d30bcfc65049932e12ce79b1728e8
Vladimir <kusakinvova@gmail.com>
2021-10-28 13:03:48 +0400

    feat: add audio player (part2) (Thu, Oct 28, 2021 09:03:48 +0000)

--- momentum/assets/js/audioplayer.js
+++ momentum/assets/js/audioplayer.js
@@ -15,25 +15,79 @@ export function audioPlayer(className) {
     playlistBlock.append(e);
   });
   //------------------------------------------------------
+  const tracks = document.querySelectorAll('.play-item');
+  tracks[trackPlay].classList.add('item-active');
+  tracks.forEach(track => {
+    track.addEventListener('click', event => {
+
+
+
+      const track_click = +track.getAttribute('track_point');
+      if(trackPlay !== track_click){
+        audio.src = playList[track_click].src;
+        trackPlay = track_click;
+        playAudio();
+      }
+      else{
+        if(audioPlayNow) pauseAudio();
+        else playAudio();
+      }
+      checkActiveTrack(track_click);
+      // track.classList.add('item-active');
+      /*
+      const videosource = track.getAttribute('track_point');
+      const videoPositionNew = item.getAttribute('data-position');
+      if(videoPlay !== videosource){
+        let elements = document.getElementsByClassName('video__item_active');
+        while(elements.length > 0){
+          elements[0].classList.remove('video__item_active');
+        }
+        videoPosition = videoPositionNew;
+
+        video.setAttribute('src', videosource);
+        videoPlay = videosource;
+        item.classList.add('video__item_active');
+        togglePlay();
+      }
+      */
+    })
+  });
+  function checkActiveTrack(current){
+    for (let i = 0; i < tracks.length; i++) {
+      tracks[i].classList.remove('item-active');
+    }
+    tracks[current].classList.add('item-active');
+  }
+  //------------------------------------------------------
 
   const buttonPlay = document.querySelector(className + ' .play');
   const buttonPlayPrev = document.querySelector(className + ' .play-prev');
   const buttonPlayNext = document.querySelector(className + ' .play-next');
-  buttonPlay.addEventListener("click", playAudio);
+  buttonPlay.addEventListener("click", togglePlayAudio);
   buttonPlayPrev.addEventListener("click", playAudioPrev);
   buttonPlayNext.addEventListener("click", playAudioNext);
 
 
 
-  function playAudio() {
-    buttonPlay.classList.toggle("pause");
+  function togglePlayAudio() {
+    // buttonPlay.classList.toggle("pause");
     if (!audioPlayNow) {
-      audio.play();
-      audioPlayNow = true;
+      playAudio();
     } else {
-      audio.pause();
-      audioPlayNow = false;
+      pauseAudio();
     }
+  }
+
+  function playAudio(){
+    audio.play();
+    audioPlayNow = true;
+    buttonPlay.classList.add("pause");
+  }
+
+  function pauseAudio(){
+    audio.pause();
+    audioPlayNow = false;
+    buttonPlay.classList.remove("pause");
   }
 
   function playAudioPrev() {
@@ -41,7 +95,9 @@ export function audioPlayer(className) {
     else trackPlay --;
     audio.src = playList[trackPlay].src;
     audio.play();
+    checkActiveTrack(trackPlay);
     console.log(playList[trackPlay].src);
+
   }
 
   function playAudioNext(){
@@ -49,6 +105,7 @@ export function audioPlayer(className) {
     else trackPlay ++;
     audio.src = playList[trackPlay].src;
     audio.play();
+    checkActiveTrack(trackPlay);
     console.log(playList[trackPlay].src);
   }
 
