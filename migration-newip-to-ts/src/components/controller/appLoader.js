b3638478e2ae8690c140810b82b0751e090d5597
Vladimir <kusakinvova@gmail.com>
2021-12-05 15:20:04 +0400

    feat: add api-Key newsapi

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'e6b5df2b8a6d4c8db96b5b1a54d48747', // получите свой ключ https://newsapi.org/
         });
     }
 }
