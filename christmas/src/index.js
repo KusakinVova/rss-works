360b8667c26a8766248b871055dae69c4762197e
Vladimir <kusakinvova@gmail.com>
2021-12-30 19:47:51 +0400

    feat: add snow

--- christmas/src/index.js
+++ christmas/src/index.js
@@ -43,12 +43,13 @@ function goSnow() {
 
 function createSnowFlake() {
   const snowFlake = document.createElement('i');
-  snowFlake.classList.add('fas');
-  snowFlake.classList.add('fa-snowflake');
+  snowFlake.classList.add('snowflake');
   snowFlake.style.left = Math.random() * window.innerWidth + 'px';
   snowFlake.style.animationDuration = Math.random() * 3 + 2 + 's'; // between 2 - 5 seconds
   snowFlake.style.opacity = Math.random();
-  snowFlake.style.fontSize = Math.random() * 10 + 10 + 'px';
+  const size = Math.random() * 10 + 10 + 'px';
+  snowFlake.style.width = size;
+  snowFlake.style.height = size;
   document.body.appendChild(snowFlake);
   setTimeout(() => {
     snowFlake.remove();
